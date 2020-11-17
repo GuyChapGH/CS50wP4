9e8ec31394644d6bef02a77fadeec67e170df06f
Guy Chaplin <guy.chaplin@googlemail.com>
2020-11-17 13:04:30 +0000

    defines Post and Likes models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="poster")
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.content} at {self.timestamp}"
+
+
+class Likes(models.Model):
+    post = models.OneToOneField("Post", on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.post.id}: Likes: {self.count}"
