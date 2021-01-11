89dc588535030f1fef67031e47180484c65dcb94
Guy Chaplin <guy.chaplin@googlemail.com>
2021-01-11 13:26:36 +0000

    starts to add edit link to all posts in index.html

---
+++ network/static/network/edit.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //console.log("hello world!");
+    //alert("Hello, world!");
+
+    //const post = document.querySelector('.allpost');
+    //console.log(post);
+
+    // Select the posts from the document
+    var posts = document.getElementsByClassName("allpost");
+    //console.log(posts);
+
+    // Convert HTMLCollection into an array
+    var posts_arr = Array.from(posts);
+    console.log(posts_arr);
+
+    // Iterate through array of allpost divs
+    posts_arr.forEach(add_edit_link);
+
+    // TRY to add edit link to each allpost div. (NOT WORKING).
+    function add_edit_link (div_content) {
+        var a = document.createElement('a');
+        var linkText = document.createTextNode("Edit");
+        a.appendChild(linkText);
+        a.title = "Edit";
+        a.href = "#";
+        //document.querySelector('.allpost').appendChild(a);
+        document.body.appendChild(a);
+        //console.log (div_content);
+
+
+    }
+
+});
